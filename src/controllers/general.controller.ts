244d1e93296f208eef5ae518b9878a9b2cfd7d97
Sheroz Sharipov <32864897+SherozSahripov@users.noreply.github.com>
2022-07-06 12:56:52 +0300

    Update general.controller.ts

--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -4,7 +4,11 @@ export const generalController = async (req: Request, res: Response) => {
   try {
     // const { topNo } = req.query;
     const response = await generalService(req.query);
-
+    if (!response) {
+      return res.status(404).json({
+        message: "No data found",
+      });
+    }
     return res.send(response);
   } catch (error) {
     console.log(error);
